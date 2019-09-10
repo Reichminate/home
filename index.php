bcaba00a4bae8aa5ec3b8e84ba02b9ea0de9b4e2
Tosca Yoel Connery <toscayoelconnery@gmail.com>
2019-09-10 23:39:27 +0700

    new color on service box

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
 
       </div>
     </section><!-- #about -->
-
+`
     <!--==========================
       Services Section
     ============================-->
@@ -153,15 +153,15 @@
         <div class="row">
 
           <div class="col-lg-6">
-            <div class="box wow fadeInLeft">
+            <div class="box shine wow fadeInLeft" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Company Profile</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
+              <p class="description" style="overflow:hidden;">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident etiro rabeta lingo.</p>
             </div>
           </div>
 
           <div class="col-lg-6">
-            <div class="box wow fadeInRight">
+            <div class="box shine wow fadeInRight" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Web Development</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
@@ -169,7 +169,7 @@
           </div>
 
           <div class="col-lg-6">
-            <div class="box wow fadeInRight">
+            <div class="box shine wow fadeInRight" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Search Engine Optimization</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
@@ -177,7 +177,7 @@
           </div>
 
           <div class="col-lg-6">
-            <div class="box wow fadeInRight">
+            <div class="box shine wow fadeInRight" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Content Creation</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
@@ -185,7 +185,7 @@
           </div>
 
           <div class="col-lg-6">
-            <div class="box wow fadeInRight">
+            <div class="box shine wow fadeInRight" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Social Media Management</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
@@ -193,52 +193,12 @@
           </div>
 
           <div class="col-lg-6">
-            <div class="box wow fadeInRight">
+            <div class="box shine wow fadeInRight" style="overflow:hidden;">
               <div class="icon"><i class="fa fa-bar-chart"></i></div>
               <h4 class="title"><a href="">Digital Advertising</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
             </div>
           </div>
-
-          <!-- <div class="col-lg-6">
-            <div class="box wow fadeInRight">
-              <div class="icon"><i class="fa fa-bar-chart"></i></div>
-              <h4 class="title"><a href="">Ecommerce Solutions</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
-            </div>
-          </div> -->
-
-          <!-- <div class="col-lg-6">
-            <div class="box wow fadeInRight">
-              <div class="icon"><i class="fa fa-bar-chart"></i></div>
-              <h4 class="title"><a href="">Search Engine Optimization</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="box wow fadeInRight">
-              <div class="icon"><i class="fa fa-bar-chart"></i></div>
-              <h4 class="title"><a href="">ERP System</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata nodera clas.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="box wow fadeInLeft" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-shopping-bag"></i></div>
-              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur trinige zareta lobur trade.</p>
-            </div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="box wow fadeInRight" data-wow-delay="0.2s">
-              <div class="icon"><i class="fa fa-map"></i></div>
-              <h4 class="title"><a href="">Magni Dolores</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum rideta zanox satirente madera</p>
-            </div>
-          </div> -->
 
         </div>
 
